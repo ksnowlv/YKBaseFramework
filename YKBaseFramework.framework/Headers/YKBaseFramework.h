0010158898f5526c30900e80d903728b33176d1a
ksnowlv <ksnowlv@163.com>
2018-07-05 11:00:55 +0800

    YKBaseFramework

---
+++ YKBaseFramework.framework/Headers/YKBaseFramework.h
@@ -0,0 +1,17 @@
+//
+//  YKBaseFramework.h
+//  YKBaseFramework
+//
+//  Created by ksnowlv on 2018/7/2.
+//  Copyright © 2018年 ksnowlv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YKBaseFramework.
+FOUNDATION_EXPORT double YKBaseFrameworkVersionNumber;
+
+//! Project version string for YKBaseFramework.
+FOUNDATION_EXPORT const unsigned char YKBaseFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YKBaseFramework/PublicHeader.h>
